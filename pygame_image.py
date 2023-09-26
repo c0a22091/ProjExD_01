73cc14cead6f63f6d8a9a113d375275c85f07931
Tomoya <c0a2209178@edu.teu.ac.jp>
2023-09-26 15:49:20 +0900

    コミット三回目

--- pygame_image.py
+++ pygame_image.py
@@ -17,10 +17,11 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(img3,[300,200])
+        screen.blit(lst[tmr%2],[300,200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1      
         clock.tick(10)
+        
 
 
 if __name__ == "__main__":
